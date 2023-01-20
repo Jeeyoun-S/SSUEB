365e7132a1a0674d57453dc38836764a0d4ebf87
서지윤 <jeey0124@naver.com>
2023-01-20 13:25:41 +0900

    S08P12A801-44 Feat: 반려인 회원가입 구현

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,49 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="main">
+    <TheSidebar v-show="isShowSidebar"></TheSidebar>
+    <router-view />
+  </div>
 </template>
 
+<script>
+import TheSidebar from '@/views/TheSidebar.vue';
+
+export default {
+  name: 'app',
+  components: {
+    TheSidebar
+  },
+  data() {
+    return {
+      isShowSidebar: true // 사이드바가 보이면 true, 안 보이면 false
+    }
+  },
+  created() {
+    // 회원가입 페이지에서는 Sidebar가 안 보이게 설정
+    if (document.location.pathname === '/join') {
+      this.isShowSidebar = false;
+    }
+
+    //SweetAlert2 테스트
+    //this.$swal.fire('SweetAlert2 테스트입니다.')
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import "@/css/font.css";
 
-nav {
-  padding: 30px;
-}
+.main {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: left;
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  /* 가로 스크롤 설정 */
+  overflow: scroll;
+  overflow: auto;
+  white-space: nowrap;
 
-nav a.router-link-exact-active {
-  color: #42b983;
+  font-family: 'SUIT-Regular';
 }
 </style>
---
+++ frontend/src/views/LogoVer1.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-sheet elevation="2" class="mx-auto sheet logo" height="290" width="300" rounded>
+    <div class="logo-inner">
+      <img class="big" :src="logoImage" />
+      <h1>SSUEB</h1>
+    </div>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  name: 'LogoVer1',
+  data() {
+    return {
+      logoImage: require('@/assets/logo_crop.jpg')
+    }
+  }
+}
+</script>
+
+<style scoped>
+@import "@/css/logo.css";
+</style>
