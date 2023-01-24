96370048881f26afc0d9f1a7e93d1990d7705c17
Halo <87004079+Halo-oo@users.noreply.github.com>
2023-01-24 17:36:42 +0900

    Feat: 새로고침 시 로그인 유지
    - vuex-persistedstate 사용
    - npm i --save vuex-persistedstate

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,19 +1,45 @@
-import { createStore } from "vuex";
+// for. 페이지 refresh 시 vuex store 값 유지
+import Vuex from "vuex";
+
+// ! npm i --save vuex-persistedstate 설치 필요
+import createPersistedState from "vuex-persistedstate";
 
 import userStore from "@/store/modules/userStore.js";
 import communityStore from "@/store/modules/communityStore.js";
 import meetingRoomStore from "@/store/modules/meetingRoomStore.js";
 import reservationStore from "@/store/modules/reservationStore.js";
 
-export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+// for. 페이지 refresh 시 vuex store 값 유지
+// [기존 code] 주석 처리
+// import { createStore } from "vuex";
+// export default createStore({
+//   state: {},
+//   getters: {},
+//   mutations: {},
+//   actions: {},
+//   modules: {
+//     userStore: userStore,
+//     communityStore: communityStore,
+//     meetingRoomStore: meetingRoomStore,
+//     reservationStore: reservationStore,
+//   },
+// });
+
+// for. 페이지 refresh 시 vuex store 값 유지
+const store = new Vuex.Store({
   modules: {
     userStore: userStore,
     communityStore: communityStore,
     meetingRoomStore: meetingRoomStore,
     reservationStore: reservationStore,
   },
+  // vuex plugin 명시
+  plugins: [
+    createPersistedState({
+      // ! localStorage에 저장할 store만을 path에 등록
+      paths: ["userStore"],
+    }),
+  ],
 });
+
+export default store;
