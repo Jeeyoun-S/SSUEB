c7a3b36974f45905f4d565d36b8251fd7bdea1eb
Halo <87004079+Halo-oo@users.noreply.github.com>
2023-02-09 13:38:51 +0900

    Design: 404 에러 페이지 css 수정

--- frontend/src/components/ErrorPage/NotFoundPage.vue
+++ frontend/src/components/ErrorPage/NotFoundPage.vue
@@ -1,75 +1,20 @@
 <template>
-  <div class="main-page">
-    <div class="main-center">
-      <img
-        class="errorImg"
-        :src="require('@/assets/fail/universeNotFound.gif')"
-      />
-    </div>
+  <div class="main-error">
+    <img
+      class="errorImg"
+      :src="require('@/assets/fail/universeNotFound.gif')"
+    />
   </div>
-  <!-- <div class="errorImg">
-    <img :src="require('@/assets/fail/universeNotFound.gif')" />
-  </div> -->
 </template>
 
 <style scoped>
+.main-error {
+  display: flex;
+  margin-right: 30%;
+  /* margin-bottom: 20%; */
+}
 .errorImg {
-  width: 120%;
-  height: 120%;
   margin-left: 30%;
-}
-.main-page {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-start;
-}
-.main-center {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-
-  width: 730px;
-  height: 654.4px;
-  margin-right: 40px;
-}
-.main-center .main-center-item {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-
-  width: 730px;
-  height: 200px;
-}
-.main-center .main-center-item img {
-  width: 200px;
-  height: 200px;
-
-  border-radius: 10px;
-  object-fit: cover;
-}
-.main-center .main-center-item .explain {
-  width: 500px;
-  height: 200px;
-
-  border-radius: 10px;
-  padding: 35px;
-}
-.main-center .main-center-item .explain h2 {
-  font-size: 30px;
-  font-family: "Patua One", cursive;
-  font-weight: lighter;
-}
-.main-center .main-center-item .explain.one {
-  background-color: #e8ebf6;
-}
-.main-center .main-center-item .explain.two {
-  color: white;
-  background-color: #3d6ec5;
-}
-.main-page .main-center .main-center-item .explain.three {
-  background-color: #e3e3e3;
+  width: 100%;
 }
 </style>
