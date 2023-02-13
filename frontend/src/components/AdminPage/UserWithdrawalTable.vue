706f16c2eed003bb7c091a0470a4c5ff5a2c2a08
Halo <87004079+Halo-oo@users.noreply.github.com>
2023-02-14 08:50:44 +0900

    Feat: 관리자 - 회원탈퇴 일부 구현

--- frontend/src/components/AdminPage/UserWithdrawalTable.vue
+++ frontend/src/components/AdminPage/UserWithdrawalTable.vue
@@ -2,7 +2,9 @@
   <v-sheet class="mr-2" width="150">
     <v-combobox
       :items="['이메일', '이름', '닉네임']"
-      variant="outlined" density="compact" v-model="range"
+      variant="outlined"
+      density="compact"
+      v-model="range"
       hide-details
     ></v-combobox>
   </v-sheet>
@@ -28,16 +30,22 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="(user, index) in userList" :key="index"
-        style="cursor: pointer;"
+      <tr
+        v-for="(user, index) in userList"
+        :key="index"
+        style="cursor: pointer"
       >
-        <td>{{ String(index).padStart(4, '0') }}</td>
+        <td>{{ String(index).padStart(4, "0") }}</td>
         <td>{{ user.id }}</td>
         <td>{{ user.userName }}</td>
         <td>{{ user.userNickname }}</td>
         <td>{{ user.authorities[0].authorityName.substr(5, 10) }}</td>
         <td>
-          <v-btn class="mr-3" variant="outlined" color="error" rounded="0"
+          <v-btn
+            class="mr-3"
+            variant="outlined"
+            color="error"
+            rounded="0"
             @click="withdrawal(user.id, user.userIsSocialId)"
           >
             탈퇴
@@ -55,36 +63,37 @@ import { mapActions } from "vuex";
 export default {
   name: "UserWithdrawalTable",
   props: {
-    userList: Array
+    userList: Array,
   },
   data() {
     return {
-      range: "이메일"
-    }
+      range: "이메일",
+      keyword: "",
+    };
   },
   methods: {
     ...mapActions(userStore, ["excuteWithdrawal"]),
     // [@Method] 회원 탈퇴
     async withdrawal(id, userIsSocialId) {
-      userIsSocialId
-      this.$swal.fire({
-        title: "탈퇴하시겠습니까?",
-        text: `탈퇴 시 ${id} 계정은 사용하지 못하게 됩니다.`,
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes",
-      }).then(async (result) => {
-        if (result.isConfirmed) {
-          await this.excuteWithdrawal();
-        }
-      });
+      userIsSocialId;
+      this.$swal
+        .fire({
+          title: "탈퇴하시겠습니까?",
+          text: `탈퇴 시 ${id} 계정은 사용하지 못하게 됩니다.`,
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes",
+        })
+        .then(async (result) => {
+          if (result.isConfirmed) {
+            await this.excuteWithdrawal();
+          }
+        });
     },
-  }
-}
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
