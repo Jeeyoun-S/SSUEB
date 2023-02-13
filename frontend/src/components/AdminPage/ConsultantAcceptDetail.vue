7e7d562ad65c89ddd7232d316d99699484baab05
서지윤 <jeey0124@naver.com>
2023-02-13 18:01:38 +0900

    Feat: 관리자 페이지

--- frontend/src/components/AdminPage/ConsultantAcceptDetail.vue
+++ frontend/src/components/AdminPage/ConsultantAcceptDetail.vue
@@ -1,74 +1,3 @@
-<!-- <template>
-  <NowLoading v-if="!loaded"></NowLoading>
-  <div v-else class="page max-page border-sheet-four">
-    <div class="page-inner max-page">
-      <div class="page-inner-title border-sheet-four">
-        <v-btn
-          class="mr-1"
-          variant="text"
-          icon="mdi-arrow-left"
-          @click="back()"
-        ></v-btn>
-        <v-chip class="mr-3" color="primary">
-          {{ String(getConsultantDetail.index).padStart(4, "0") }}
-        </v-chip>
-        <h2>{{ getConsultantDetail.userName }}</h2>
-      </div>
-    </div>
-    <div class="pa-8 d-flex flex-column page-inner-items border-sheet-four">
-      <v-table>
-        <tbody>
-          <tr>
-            <td class="bold-font" width="150">이름</td>
-            <td width="920">{{ getConsultantDetail.userName }}</td>
-          </tr>
-          <tr>
-            <td class="bold-font" width="150">가입 날짜</td>
-            <td width="920">{{ getConsultantDetail.userJoindate }}</td>
-          </tr>
-          <tr>
-            <td class="bold-font" width="150">이메일</td>
-            <td width="920">{{ getConsultantDetail.id }}</td>
-          </tr>
-          <tr>
-            <td class="bold-font" width="150">휴대폰 번호</td>
-            <td width="920">{{ getConsultantDetail.userPhone }}</td>
-          </tr>
-          <tr>
-            <td class="bold-font" width="150">상담 가능한 동물</td>
-            <td width="920">{{ getConsultantDetail.consultantPetType }}</td>
-          </tr>
-          <tr>
-            <td class="bold-font" width="150">자격 번호</td>
-            <td width="920">
-              {{ getConsultantDetail.consultantLicenseNumber }}
-            </td>
-          </tr>
-          <tr>
-            <td class="bold-font" width="150">자격증 사본</td>
-            <td>
-              <v-btn
-                rounded="pill"
-                variant="tonal"
-                prepend-icon="mdi-paperclip"
-              >
-                {{ getConsultantDetail.consultantLicenseCopyImage }}
-              </v-btn>
-            </td>
-          </tr>
-          <tr>
-            <td class="bold-font" width="150">가입 수락</td>
-            <td>
-              <v-btn rounded="pill" variant="tonal" @click="accept">
-                수락
-              </v-btn>
-            </td>
-          </tr>
-        </tbody>
-      </v-table>
-    </div>
-  </div>
-</template> -->
 <template>
   <NowLoading v-if="!loaded"></NowLoading>
   <div v-else class="page max-page border-sheet-four">
@@ -91,21 +20,21 @@
         <tbody>
           <tr>
             <td class="bold-font" width="150">이름</td>
-            <td width="920">{{ this.consultantDetail.user_name }}</td>
+            <td width="900">{{ this.consultantDetail.user_name }}</td>
           </tr>
           <tr>
             <td class="bold-font" width="150">가입 날짜</td>
-            <td width="920">
+            <td width="900">
               {{ this.consultantDetail.user_joindate.substring(0, 10) }}
             </td>
           </tr>
           <tr>
             <td class="bold-font" width="150">이메일</td>
-            <td width="920">{{ this.consultantDetail.id }}</td>
+            <td width="900">{{ this.consultantDetail.id }}</td>
           </tr>
           <tr>
             <td class="bold-font" width="150">휴대폰 번호</td>
-            <td width="920">
+            <td width="900">
               {{ this.consultantDetail.user_phone.substring(0, 3) }}-{{
                 this.consultantDetail.user_phone.substring(3, 7)
               }}-{{ this.consultantDetail.user_phone.substring(7, 11) }}
@@ -134,17 +63,16 @@
           </tr>
           <tr>
             <td class="bold-font" width="150">자격 번호</td>
-            <td width="920">
+            <td width="900">
               {{ this.consultantDetail.consultant_license_number }}
             </td>
           </tr>
           <tr>
             <td class="bold-font" width="150">자격증 사본</td>
             <td>
-              <v-btn
-                rounded="pill"
-                variant="tonal"
-                prepend-icon="mdi-paperclip"
+              <v-btn :href="getLicenseUrl(consultantDetail.consultant_license_copy_image)" 
+                rounded="pill" variant="tonal" prepend-icon="mdi-paperclip"
+                download
               >
                 {{ this.consultantDetail.consultant_license_copy_image }}
               </v-btn>
@@ -153,7 +81,9 @@
           <tr>
             <td class="bold-font" width="150">가입 수락</td>
             <td>
-              <v-btn rounded="pill" variant="tonal" @click="accept">
+              <v-btn rounded="pill" variant="tonal" @click="accept"
+                color="primary"
+              >
                 수락
               </v-btn>
             </td>
@@ -193,6 +123,9 @@ export default {
     accept() {
       acceptConsultant(this.getConsultantDetail.id);
     },
+    getLicenseUrl(img) {
+      return `${process.env.VUE_APP_FILE_PATH_LICENSE}${img}`;
+    }
   },
 };
 </script>
