8605e6c4526c5894e5890634bb72ecdb43b4fbff
MingyuGhim <109265235+MingyuGhim@users.noreply.github.com>
2023-02-14 16:46:29 +0900

    Update SeeAttatchedFiles.vue

--- frontend/src/components/SeeAttachedFiles/SeeAttatchedFiles.vue
+++ frontend/src/components/SeeAttachedFiles/SeeAttatchedFiles.vue
@@ -1,35 +1,39 @@
 <template>
-  <v-dialog v-model="dialog" width="800">
-    <template v-slot:activator="{ props }">
-      <v-btn variant="text" color="primary" v-bind="props" @click="seefile">
-        <!--block :disabled="reservation.matchingConsultants.length < 1" -->
-        관련 첨부파일 보기
-      </v-btn>
-    </template>
+  <v-row>
+    <v-dialog v-model="dialog" width="800" scrollable>
+      <template v-slot:activator="{ props }">
+        <v-btn variant="text" color="primary" v-bind="props" @click="seefile">
+          <!--block :disabled="reservation.matchingConsultants.length < 1" -->
+          관련 첨부파일 보기
+        </v-btn>
+      </template>
 
-    <v-card>
-      <v-card-title><h3>관련 첨부파일 보기</h3></v-card-title>
-      <v-card-text class="pa-0 pl-5"> </v-card-text>
-      <v-card-item>
-        <div class="d-flex justify-center pa-5">
-          <div v-for="(url, idx) in attach" :key="idx" class="ma-2 pa-2">
-            <img :src="url" v-if="url.endsWith('jpg')" />
-            <video
-              autoplay
-              controls
-              muted
-              :src="url"
-              v-if="url.endsWith('mp4')"
-              type="video/mp4"
-            />
+      <v-card>
+        <v-card-title><h3>관련 첨부파일 보기</h3></v-card-title>
+        <v-divider></v-divider>
+        <v-card-text class="pa-0 pl-5"> </v-card-text>
+        <v-card-item>
+          <div class="pa-5">
+            <v-row v-for="(url, idx) in attach" :key="idx" class="ma-2 pa-2" justify="center">
+              <img :src="url" v-if="url.endsWith('jpg')" />
+              <video
+                autoplay
+                controls
+                muted
+                :src="url"
+                v-if="url.endsWith('mp4')"
+                type="video/mp4"
+              />
+            </v-row>
+            <br />
           </div>
-        </div>
-      </v-card-item>
-      <v-card-actions>
-        <v-btn color="primary" block @click="dialog = false">닫기</v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+        </v-card-item>
+        <v-card-actions>
+          <v-btn color="primary" block @click="dialog = false">닫기</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
 </template>
 
 <script>
