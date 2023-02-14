629a7b1160b3a5721838a63864bd87231a780200
SSZ4 <37368388+SSZ4@users.noreply.github.com>
2023-02-14 09:49:46 +0900

    Feat: Board, 댓글 관련 기능 추가

--- backend/src/main/java/com/ssafy/board/BoardReplyController.java
+++ backend/src/main/java/com/ssafy/board/BoardReplyController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,7 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @Api(tags = {"Board/Reply"}, description = "게시판 댓글 관련 API")
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/board/reply")
 public class BoardReplyController {
 	
@@ -67,7 +69,9 @@ public class BoardReplyController {
     })
 	public ResponseEntity<?> createReply(@RequestBody Reply reply) {
 		try {
+			System.out.println(reply);
 			Reply result = bService.createReply(reply);
+			System.out.println(result);
 			return new ResponseEntity<Reply>(result, HttpStatus.OK);
 		} catch (Exception e) {
 			return exceptionHandling(e);
