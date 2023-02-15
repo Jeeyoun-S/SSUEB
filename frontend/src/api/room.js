e8c2d7a002089b41fa04201d655d1a50ed3187da
yeonso16385 <yeonso16385@naver.com>
2023-02-15 21:28:04 +0900

    0215 frontend: linux환경에 맞게 수정

--- frontend/src/api/room.js
+++ frontend/src/api/room.js
@@ -7,8 +7,8 @@ async function joinRoomSession(roomId, success, fail) {
     //나중에 수정. login 정보와 기타등등을 넘겨야함.
     // console.log(loginInfo);d
     await api
-      // .post(`${process.env.VUE_APP_API_BASE_URL}/room/session`, {customSessionId: roomId},{  
-      .post(`api/room/session`, {customSessionId: roomId},{  
+      .post(`${process.env.VUE_APP_API_BASE_URL}/room/session`, {customSessionId: roomId},{  
+      // .post(`api/room/session`, {customSessionId: roomId},{  
           headers: {
             Authorization: `Bearer ${token}`
           },
@@ -19,8 +19,8 @@ async function joinRoomSession(roomId, success, fail) {
 
 async function finishRoom(roomId, success,fail){
   await api
-  // .post(`${process.env.VUE_APP_API_BASE_URL}/reservation/${roomId}`,{  
-  .put(`api/reservation/finish/${roomId}`,{  
+  .post(`${process.env.VUE_APP_API_BASE_URL}/reservation/${roomId}`,{  
+  // .put(`api/reservation/finish/${roomId}`,{  
       headers: {
         Authorization: `Bearer ${token}`
       },
